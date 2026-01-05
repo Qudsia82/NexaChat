a0c9dbc2303ab03b601f3ac7a86ccba1bd190051
Qudsia Shaikh <qudsiashaikh216@gmail.com>
2026-01-05 18:01:30 +0500

    feat:implement message routes

--- backend/src/routes/messages.js
+++ backend/src/routes/messages.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { getAllChats, getAllContacts, getChatById, sendMessages } from "../controllers/message.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send",(req,res)=>{
-    res.send("Messages are sending..");
-});
+router.use(arcjetProtection,protectRoute)
+
+router.get("/contacts",getAllContacts);
+router.get("/chats",getAllChats);
+router.get("/:id",getChatById);
+router.post("/send/:id",sendMessages);
 
 export default router;
